85cebbc91cd8eef2cd6c4667f51bd4ad7ef7bf74
Andrew <fylhtqhecfrjd@gmail.com>
2024-11-29 02:23:07 +0300

    Нахуй этот девтулс

--- src/main/java/me/list_tw/referralstats/ReferralStats.java
+++ src/main/java/me/list_tw/referralstats/ReferralStats.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReferralStatsApplication {
+public class ReferralStats {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReferralStatsApplication.class, args);
+        SpringApplication.run(ReferralStats.class, args);
     }
-
 }
